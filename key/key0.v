fa29c55bafc7d8362dd384097c24d1aa11a0e263
zemzhao <zemzhao@163.com>
2021-07-28 13:55:04 +0800

    key done

---
+++ key/key0.v
@@ -0,0 +1,69 @@
+///////////////////////////////////////////
+// key.v:
+// 具有防抖功能和连按功能
+// 防抖：当按下之后超高10ms后还保持按键状态则为确认按下
+// 连按：当确认按下490ms后则自动识别为抬起，确认按下990ms后开始检测第二次按键
+///////////////////////////////////////////
+module key(
+    input clk, 
+    input key,
+    
+    output fs
+);
+
+    localparam MS10 = 32'd500_000;
+    localparam MS490 = 32'd24_500_000;
+    localparam MS500 = 32'd25_000_000;
+
+    localparam IDLE = 8'h00, WAIT0 = 8'h01, CHECK0 = 8'h02;
+    localparam START = 8'h03, CHECK1 = 8'h04, WAIT1 = 8'h05;
+
+    reg [7:0] state, next_state;
+
+    reg [31:0] cnt;
+
+    assign fs = (state == START);
+
+
+    always @(posedge clk) begin
+        state <= next_state;
+    end
+
+    always@(*) begin
+        case(state)
+            IDLE: begin
+                if(~key) next_state <= WAIT0;
+                else next_state <= IDLE;
+            end
+            WAIT0: begin
+                if(cnt == MS10) next_state <= CHECK0;
+                else next_state <= WAIT0;
+            end
+            CHECK0: begin
+                if(~key) next_state <= START;
+                else next_state <= IDLE;
+            end
+            START: begin
+                if(cnt == MS490) next_state <= CHECK1;
+                else next_state <= START;
+            end
+            CHECK1: begin
+                next_state <= WAIT1;
+            end
+            WAIT1: begin
+                if(cnt == MS500) next_state <= IDLE;
+                else next_state <= WAIT1;
+            end
+            default: next_state <= IDLE;
+        endcase
+    end
+
+    always @(posedge clk) begin
+        if(state == IDLE || state == CHECK0 || state == CHECK1) cnt <= 32'h0;
+        else if(state == WAIT0 || state == START || state == WAIT1) cnt <= cnt + 1'b1;
+        else cnt <= cnt;
+    end
+
+
+
+endmodule
--- key/top.v
+++ key/top.v
@@ -1,21 +1,60 @@
 module top(
-    input sys_clk,
+    input clk,
 
     input [3:0] key,
-    output [3:0] led
+    output [3:0] lec,
+    output [31:0] led
+
 );
 
+    localparam VERSION = 8'h02;
+    assign led[31:24] = ~VERSION;
+
+    reg [7:0] state, next_state;
     wire fs;
-    assign led[0] = ~fs;
-    assign led[3:1] = 3'b111;
+    reg [3:0] led_cont;
 
-    key 
-    key_dut (
-        .clk(sys_clk),
-        .key(key),
-        .fs(fs),
-        .fd()
+    localparam IDLE = 8'h00, WORK = 8'h01, LAST = 8'h02;
+    localparam START = 8'h03;
+
+    assign lec = ~led_cont;
+
+    always @(posedge clk) begin
+        state <= next_state;
+    end
+
+    always @(*) begin
+        case(state)
+            IDLE: begin
+                next_state <= START;
+            end
+            START: begin
+                if(fs) next_state <= WORK;
+                else next_state <= START;
+            end
+            WORK: begin
+                next_state <= LAST;
+            end
+            LAST: begin
+                if(~fs) next_state <= START;
+                else next_state <= LAST;
+            end
+        endcase
+    end
+
+    always @(posedge clk) begin
+        if(state == IDLE) led_cont <= 4'h0;
+        else if(state == WORK) led_cont <= led_cont + 1'b1;
+        else led_cont <= led_cont;
+    end
+
+
+
+    key
+    key_dut(
+        .clk(clk),
+        .key(key[0]),
+        .fs(fs)
     );
-
 
 endmodule
