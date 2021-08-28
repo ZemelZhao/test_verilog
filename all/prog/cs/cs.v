58cb5f448d1f6ec8b9d75ad87e948117f0c25a82
zemzhao <zemzhao@163.com>
2021-08-28 21:23:31 +0800

    cs done

---
+++ all/prog/cs/cs.v
@@ -0,0 +1,225 @@
+module cs(
+    // CLK
+    input osc_clk,
+    input eth_clk,
+    output sys_clk,
+    output fifo_clk,
+    output gmii_rxc,
+    output gmii_txc,
+    output spi_mclk,
+    output fifoa_txc,
+    output fifoa_rxc,
+    output fifoc_txc,
+    output fifoc_rxc,
+    output fifod_txc,
+    output fifod_rxc,
+
+    // RST
+    input rst_sys,
+    output rst_mac,
+    output rst_adc,
+    output rst_fifoc,
+    output rst_fifod,
+    output rst_mac2fifoc,
+    output rst_fifoc2cs,
+    output rst_adc2fifod,
+    output rst_fifod2mac,
+
+    // TEST
+    input adc_rxc,
+    input [3:0] dev_com,
+
+    // ERR
+
+    // ADC REG
+    input [7:0] cmd_filt,
+    input [7:0] cmd_mix0,
+    input [7:0] cmd_mix1,
+    input [7:0] cmd_reg4,
+    input [7:0] cmd_reg5,
+    input [7:0] cmd_reg6,
+    input [7:0] cmd_reg7,
+
+    output [7:0] adc_reg00,
+    output [7:0] adc_reg01,
+    output [7:0] adc_reg02,
+    output [7:0] adc_reg03,
+    output [7:0] adc_reg04,
+    output [7:0] adc_reg05,    
+    output [7:0] adc_reg06,
+    output [7:0] adc_reg07,
+    output [7:0] adc_reg08,
+    output [7:0] adc_reg09,
+    output [7:0] adc_reg10,
+    output [7:0] adc_reg11,
+    output [7:0] adc_reg12,
+    output [7:0] adc_reg13,
+    output [7:0] adc_regap,
+
+    // FIFO
+    input fifoc_full,
+    input fifod_full,
+    input fifoa_full,
+
+    // FLAG
+    input fs_udp_rx,
+    output fs_mac2fifoc,
+    output fs_fifoc2cs,
+    output fd_udp_rx,
+    input fd_mac2fifoc,
+    input fd_fifoc2cs,
+
+    output fs_udp_tx,
+    output fs_fifod2mac,
+    output fd_udp_tx,
+    input fd_fifod2mac,
+
+    output fs_adc_check,
+    output fs_adc_conf,
+    output fs_adc_read,
+    output fs_adc_fifo,
+    input fd_adc_check,
+    input fd_adc_conf,
+    input fd_adc_read,
+    input fd_adc_fifo,
+
+    // NUM
+    input [7:0] info_sr,
+    input [7:0] kind_dev,
+    output [7:0] dev_info,
+
+    output [9:0] adc_rx_len,
+    output [11:0] eth_tx_len,
+
+    output [63:0] intan_cmd, 
+    output [63:0] intan_ind,
+    output [7:0] intan_lrt,
+    output [7:0] intan_end
+);
+
+    // RUN PARAMETER
+
+    wire [3:0] dev_id;
+    wire [3:0] dat_id;
+    wire dev_grp;
+    wire [7:0] adc_cnt;
+
+    // CLOCK
+    wire fs_adc;
+
+    // RST
+    wire rst_all;
+    wire rst_run;
+    wire rst_cs_com;
+
+    // 
+    assign dev_info = {dev_id, dat_id};
+    assign sys_clk = osc_clk;
+
+
+    cs_reg 
+    cs_reg_dut(
+        .cmd_filt(cmd_filt),
+        .cmd_mix0(cmd_mix0),
+        .cmd_mix1(cmd_mix1),
+        .cmd_reg4(cmd_reg4),
+        .cmd_reg5(cmd_reg5),
+        .cmd_reg6(cmd_reg6),
+        .cmd_reg7(cmd_reg7),
+        .reg00(adc_reg00),
+        .reg01(adc_reg01),
+        .reg02(adc_reg02),
+        .reg03(adc_reg03),
+        .reg04(adc_reg04),
+        .reg05(adc_reg05),
+        .reg06(adc_reg06),
+        .reg07(adc_reg07),
+        .reg08(adc_reg08),
+        .reg09(adc_reg09),
+        .reg10(adc_reg10),
+        .reg11(adc_reg11),
+        .reg12(adc_reg12),
+        .reg13(adc_reg13),
+        .regap(adc_regap),
+        .dev_id(dev_id),
+        .dev_grp(dev_grp)
+    );
+
+    cs_num 
+    cs_num_dut(
+        .cmd_kdev(kind_dev),
+        .adc_rx_len(adc_rx_len),
+        .eth_tx_len(eth_tx_len),
+        .data_cnt(adc_cnt),
+        .intan_cmd(intan_cmd),
+        .intan_ind(intan_ind),
+        .intan_lrt(intan_lrt),
+        .intan_end(intan_end)
+    );
+
+    cs_com
+    cs_com_dut(
+        .sys_clk(sys_clk),
+        .rst(rst_cs_com),
+        .adc_rxc(adc_rxc),
+        .com0(dev_com[3:2]),
+        .com1(dev_com[1:0]),
+        .fd_adc_conf(fd_adc_conf),
+        .dev_grp(dev_grp),
+        .fs_adc(fs_adc),
+        .dat_id(dat_id)
+    );
+
+    cs_cmd
+    cs_cmd_dut(
+        .sys_clk(sys_clk),
+        .rst_sys(rst_sys),
+        .rst_all(rst_all),
+        .rst_run(rst_run),
+
+        .fifoa_full(fifoa_full),
+        .fifoc_full(fifoc_full),
+        .fifod_full(fifod_full),
+
+        .fs_adc(fs_adc),
+        .adc_cnt(adc_cnt),
+
+        .fs_udp_rx(fs_udp_rx),
+        .fs_mac2fifoc(fs_mac2fifoc),
+        .fs_fifoc2cs(fs_fifoc2cs),
+        .fd_udp_rx(fd_udp_rx),
+        .fd_mac2fifoc(fd_mac2fifoc),
+        .fd_fifoc2cs(fd_fifoc2cs),
+
+        .fs_udp_tx(fs_udp_tx),
+        .fs_fifod2mac(fs_fifod2mac),
+        .fd_udp_tx(fd_udp_tx),
+        .fd_fifod2mac(fd_fifod2mac),
+
+        .fs_adc_check(fs_adc_check),
+        .fs_adc_conf(fs_adc_conf),
+        .fs_adc_read(fs_adc_read),
+        .fs_adc_fifo(fs_adc_fifo),
+        .fd_adc_check(fd_adc_check),
+        .fd_adc_conf(fd_adc_conf),
+        .fd_adc_read(fd_adc_read),
+        .fd_adc_fifo(fd_adc_fifo)
+    );
+
+    cs_rst
+    cs_rst_dut(
+        .rst_all(rst_all),
+        .rst_run(rst_run),
+
+        .rst_cs_com(rst_cs_com),
+        .rst_mac(rst_mac),
+        .rst_adc(rst_adc),
+        .rst_fifoc(rst_fifoc),
+        .rst_fifod(rst_fifod),
+        .rst_mac2fifoc(rst_mac2fifoc),
+        .rst_fifoc2cs(rst_fifoc2cs),
+        .rst_adc2fifod(rst_adc2fifod),
+        .rst_fifod2mac(rst_fifod2mac)
+    );
+
+endmodule
---
+++ all/prog/cs/cs_cmd.v
@@ -0,0 +1,241 @@
+module cs_cmd(
+    input sys_clk,
+
+    input rst_sys,
+    output rst_all,
+    output rst_run,
+
+    input fs_adc,
+    input [7:0] adc_cnt,
+
+    input fifoa_full,
+    input fifoc_full,
+    input fifod_full,
+
+    input fs_udp_rx,
+    output fs_mac2fifoc,
+    output fs_fifoc2cs,
+    output fd_udp_rx,
+    input fd_mac2fifoc,
+    input fd_fifoc2cs,
+
+    output fs_udp_tx,
+    output fs_fifod2mac,
+    output fd_udp_tx,
+    input fd_fifod2mac,
+
+    output fs_adc_check,
+    output fs_adc_conf,
+    output fs_adc_read,
+    output fs_adc_fifo,
+    input fd_adc_check,
+    input fd_adc_conf,
+    input fd_adc_read,
+    input fd_adc_fifo
+);
+
+    localparam MAIN_IDLE = 8'h00, MAIN_RESET = 8'h01, MAIN_INIT = 8'h02, MAIN_WORK = 8'h03;
+
+    localparam INIT_IDLE = 8'h00, INIT_FFCK = 8'h01, INIT_UTOF = 8'h02, INIT_FTOC = 8'h03;
+    localparam INIT_URXD = 8'h04, INIT_ADCK = 8'h05, INIT_INIT = 8'h06, INIT_CONF = 8'h07;
+    localparam INIT_LAST = 8'h08;
+
+
+    localparam ADC_IDLE = 8'h00, ADC_WAIT = 8'h01, ADC_READ = 8'h02, ADC_FIFO = 8'h03;
+    localparam ADC_LAST = 8'h04;
+
+    localparam ETH_IDLE = 8'h00, ETH_WAIT = 8'h01, ETH_CAL0 = 8'h02, ETH_CAL1 = 8'h03;
+    localparam ETH_SEND = 8'h04;
+
+    localparam CMD_NUM = 8'h04;
+
+    // FIFO
+    wire fifo_full;
+
+    // CMD
+    wire fs_init, fd_init;
+    wire fs_work, fd_work;
+
+    reg [7:0] main_state, next_main_state;
+    reg [7:0] init_state, next_init_state;
+    reg [7:0] adc_state, next_adc_state;
+    reg [7:0] eth_state, next_eth_state;
+
+    // 
+    reg [7:0] adc_num;
+    reg [7:0] cmd_num;
+    reg prev_fs_adc;
+
+    assign fs_init = (main_state == MAIN_INIT);
+    assign fs_work = (main_state == MAIN_WORK);
+    assign fd_init = (init_state == INIT_LAST);
+    
+    assign fs_mac2fifoc = (init_state == INIT_UTOF);
+    assign fs_fifoc2cs = (init_state == INIT_FTOC);
+    assign fd_udp_rx = (init_state == INIT_URXD);
+    
+    assign fs_adc_check = (init_state == INIT_ADCK);
+    assign fs_adc_conf = (init_state == INIT_CONF);
+    assign fs_adc_read = (adc_state == ADC_READ);
+    assign fs_adc_fifo = (adc_state == ADC_FIFO);
+
+    assign fs_fifod2mac = (eth_state == ETH_SEND);
+    assign fs_udp_tx = fs_fifod2mac;
+    assign fd_udp_tx = fd_fifod2mac;
+
+
+    assign fifo_full = |{fifoa_full, fifoc_full, fifod_full};
+    assign rst_all = rst_sys;
+    assign rst_run = rst_sys || (main_state == MAIN_RESET);
+
+    always @(posedge sys_clk or posedge rst_all) begin
+        if(rst_all) main_state <= MAIN_IDLE;
+        else main_state <= next_main_state;
+    end
+
+    always @(posedge sys_clk or posedge rst_run) begin
+        if(rst_run) init_state <= INIT_IDLE;
+        else init_state <= next_init_state;
+    end
+
+    always @(posedge sys_clk or posedge rst_run) begin
+        if(rst_run) adc_state <= ADC_IDLE;
+        else adc_state <= next_adc_state;
+    end
+
+    always @(posedge sys_clk or posedge rst_run) begin
+        if(rst_run) eth_state <= ETH_IDLE;
+        else eth_state <= next_eth_state;
+    end
+
+    always @(*) begin
+        case(main_state)
+            MAIN_IDLE: begin
+                if(fs_udp_rx) next_main_state <= MAIN_RESET;
+                else next_main_state <= MAIN_IDLE;
+            end
+            MAIN_RESET: begin
+                if(cmd_num == CMD_NUM) next_main_state <= MAIN_INIT;
+                else next_main_state <= MAIN_RESET;
+            end
+            MAIN_INIT: begin
+                if(fd_init) next_main_state <= MAIN_WORK;
+                else next_main_state <= MAIN_INIT;
+            end
+            MAIN_WORK: begin
+                if(fs_udp_rx) next_main_state <= MAIN_IDLE;
+                else next_main_state <= MAIN_WORK;
+            end
+            default: next_main_state <= MAIN_IDLE;
+        endcase
+    end
+
+    always @(*) begin
+        case(init_state)
+            INIT_IDLE: begin
+                if(fs_init) next_init_state <= INIT_FFCK;
+                else next_init_state <= INIT_IDLE;
+            end
+            INIT_FFCK: begin
+                if(~fifo_full) next_init_state <= INIT_UTOF;
+                else next_init_state <= INIT_FFCK;
+            end
+            INIT_UTOF: begin
+                if(fd_mac2fifoc) next_init_state <= INIT_FTOC;
+                else next_init_state <= INIT_UTOF;
+            end
+            INIT_FTOC: begin
+                if(fd_fifoc2cs) next_init_state <= INIT_URXD;
+                else next_init_state <= INIT_FTOC;
+            end
+            INIT_URXD: begin
+                if(~fs_udp_rx) next_init_state <= INIT_ADCK;
+                else next_init_state <= INIT_URXD;
+            end
+            INIT_ADCK: begin
+                if(fd_adc_check) next_init_state <= INIT_INIT;
+                else next_init_state <= INIT_ADCK;
+            end
+            INIT_INIT: begin
+                next_init_state <= INIT_CONF;
+            end
+            INIT_CONF: begin
+                if(fd_adc_conf) next_init_state <= INIT_LAST;
+                else next_init_state <= INIT_CONF;
+            end
+            INIT_LAST: begin
+                if(~fs_init) next_init_state <= INIT_IDLE;
+                else next_init_state <= INIT_LAST;
+            end
+            default: next_init_state <= INIT_IDLE;
+        endcase
+    end
+
+    always @(*) begin
+        case(adc_state)
+            ADC_IDLE: begin
+                if(fs_work) next_adc_state <= ADC_WAIT;
+                else next_adc_state <= ADC_IDLE;
+            end
+            ADC_WAIT: begin
+                if({prev_fs_adc, fs_adc} == 2'b01) next_adc_state <= ADC_READ; 
+                else next_adc_state <= ADC_WAIT;
+            end
+            ADC_READ: begin
+                if(fd_adc_read) next_adc_state <= ADC_FIFO;
+                else next_adc_state <= ADC_READ;
+            end
+            ADC_FIFO: begin
+                if(fd_adc_fifo) next_adc_state <= ADC_LAST;
+                else next_adc_state <= ADC_FIFO;
+            end
+            ADC_LAST: begin
+                next_adc_state <= ADC_WAIT;
+            end
+            default: next_adc_state <= ADC_IDLE;
+        endcase
+    end
+
+    always @(*) begin
+        case(eth_state)
+            ETH_IDLE: begin
+                if(fs_work) next_eth_state <= ETH_WAIT;
+                else next_eth_state <= ETH_IDLE;
+            end
+            ETH_WAIT: begin
+                if(adc_num < adc_cnt) next_eth_state <= ETH_CAL1;
+                else next_eth_state <= ETH_CAL0;
+            end
+            ETH_CAL0: begin
+                next_eth_state <= ETH_SEND;
+            end
+            ETH_CAL1: begin
+                next_eth_state <= ETH_WAIT;
+            end
+            ETH_SEND: begin
+                if(fd_fifod2mac) next_eth_state <= ETH_WAIT;
+                else next_eth_state <= ETH_SEND; 
+            end
+            default: next_eth_state <= ETH_IDLE;
+        endcase
+    end
+
+
+    always @(posedge sys_clk) begin
+        prev_fs_adc <= fs_adc;
+    end
+
+    always @(posedge sys_clk or posedge rst_run) begin
+        if(rst_run) adc_num <= 8'h00;
+        else if(adc_state == ADC_LAST) adc_num <= adc_num + 1'b1;
+        else if(eth_state == ETH_CAL0) adc_num <= adc_num - adc_cnt;
+        else adc_num <= adc_num;
+    end
+
+    always @(posedge sys_clk or posedge rst_all) begin
+        if(rst_all) cmd_num <= 8'h00;
+        else if(main_state == MAIN_RESET) cmd_num <= cmd_num + 1'b1;
+        else cmd_num <= 8'h00;
+    end
+
+endmodule
